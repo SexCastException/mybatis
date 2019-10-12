a1939739372228bec6cff9a8be5b4e930dec2a74
pyh <739967221@qq.com>
2019-10-12 08:27:25 +0800

    ObjectFactory notes

--- mybatis-resource/mybatis-3-master/src/test/java/org/apache/ibatis/reflection/factory/DefaultObjectFactoryTest.java
+++ mybatis-resource/mybatis-3-master/src/test/java/org/apache/ibatis/reflection/factory/DefaultObjectFactoryTest.java
@@ -1,35 +1,25 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.factory;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import org.apache.ibatis.reflection.ReflectionException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 /**
  * DefaultObjectFactoryTest
@@ -48,6 +38,9 @@ class DefaultObjectFactoryTest {
     Assertions.assertEquals("foo", testClass.myString, "myString didn't match expected");
   }
 
+  /**
+   * 测试不存在的构造方法方法
+   */
   @Test
   void createClassThrowsProperErrorMsg() {
     DefaultObjectFactory defaultObjectFactory = new DefaultObjectFactory();
@@ -63,9 +56,9 @@ class DefaultObjectFactoryTest {
 
   @Test
   void creatHashMap() {
-     DefaultObjectFactory defaultObjectFactory=new DefaultObjectFactory();
-     Map  map= defaultObjectFactory.create(Map.class,null,null);
-     Assertions.assertTrue(map instanceof HashMap, "Should be HashMap");
+    DefaultObjectFactory defaultObjectFactory = new DefaultObjectFactory();
+    Map map = defaultObjectFactory.create(Map.class, null, null);
+    Assertions.assertTrue(map instanceof HashMap, "Should be HashMap");
   }
 
   @Test
